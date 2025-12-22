b2980e044d7948e9cbb48296551cdc9f6da53e88
bmahe1 <maheshreddy3585@gmail.com>
2025-12-22 15:00:47 +0530

    Delete gradle-wrapper-files directory

--- gradle-wrapper-files/gradlew.bat
+++
@@ -1,6 +0,0 @@
-@if "%DEBUG%" == "" @echo off
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem ... [file content as previously provided]
-@rem
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %CMDN_ARGS%
