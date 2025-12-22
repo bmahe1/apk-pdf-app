6e560b1f6bf87815f65e3c248a353f24a4168a78
bmahe1 <maheshreddy3585@gmail.com>
2025-12-22 14:53:02 +0530

    Update gradlew.bat

--- gradle-wrapper-files/gradlew.bat
+++ gradle-wrapper-files/gradlew.bat
@@ -1,2 +1,6 @@
-@echo off
-echo This is a placeholder gradlew.bat file. Replace with a real one or run: gradle wrapper
+@if "%DEBUG%" == "" @echo off
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem ... [file content as previously provided]
+@rem
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %CMDN_ARGS%
